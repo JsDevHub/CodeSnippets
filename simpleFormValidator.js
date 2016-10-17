b77139ed6973fc80298ce65a001b6017e74951ed
suriyaingit <suriyaingm@gmail.com>
2016-10-17 22:30:00 +0530

    Add files via upload

---
+++ simpleFormValidator.js
@@ -0,0 +1,47 @@
+	
+	function SimpleFormValidator(config){
+
+		
+var SFV = {
+ 
+    ErrorMessages  : { required : "This field is required." },
+	errorCounter : 0,
+	validateFormData: function(event){
+       SFV.errorCounter = 0;
+    
+      // prevent form submit
+      event.preventDefault();
+	 	
+	  //get the event type
+	  if(event.type == 'focusout'){
+	            var input = $(this);
+				SFV.showAndHideErrorMsg(input);
+				 	  }else{
+	        //check all input elements for null 
+			$("form#userData :input[type='text']").each(function(){
+					   var input = $(this); 
+					SFV.showAndHideErrorMsg(input);
+					 
+			 });
+	  	  }	  
+     return SFV.errorCounter;
+     
+   },
+         showAndHideErrorMsg: function(input) {
+						if(input.val().trim() == ""){
+								  input.next().show().
+								  find(".error").html(SFV.ErrorMessages.required).show();
+								  //if any null fields found increase errorCounter
+								  SFV.errorCounter++;
+					   }else{
+							  input.next().hide();
+					   }
+        } 
+
+
+}
+	 $('#'+config.formId).on('submit',SFV.validateFormData);
+   
+    $('.form-fields').on('focusout',SFV.validateFormData);
+	
+	}
