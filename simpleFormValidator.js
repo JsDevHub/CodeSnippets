03ddbc5a5d140ca247976846bf69ce5b7a43328e
JsDevHub <jsdevhub@gmail.com>
2016-10-19 15:27:54 +0530

    Update simpleFormValidator.js

--- simpleFormValidator.js
+++ simpleFormValidator.js
@@ -6,12 +6,17 @@ var SimpleFormValidator = (function() {
 
 	var validate = function(config) {
 
-		if (isNull(config)) {
+		if (!isNull(config)) {
 
 			$('#' + config.formId).on('submit', function(event) {
+			
+				if(isNull(config.rules)){
+					config.rules = getFormConfigDetails(config.formId);
+				}
+				
 				if (validateFormData(event, config) == 0) {
 
-					if (isNull(config.success))
+					if (!isNull(config.success))
 						config.success();
 
 				}
@@ -21,11 +26,41 @@ var SimpleFormValidator = (function() {
 
 	}
 
+	function getFormConfigDetails(formId) {
+
+		var req = '';
+		var msg = '';
+		var len = '';
+
+		var configDetails = [];
+
+		$("form#" + formId + " .form-fields").each(function() {
+
+			req = $(this).attr('req');
+			msg = $(this).attr('msg');
+			len = $(this).attr('len');
+
+				configDetails.push({
+					fieldName : $(this).attr('name'),
+					required : req,
+					message : msg,
+					length : len
+				});
+
+		});
+
+		console.log(configDetails)
+		
+		return configDetails;
+	}
+
+	// ----------- Validation Functions ------------ //
+
 	var validateFormData = function(event, config) {
 
 		errorCount = 0;
 
-		if (isNull(event))
+		if (!isNull(event))
 			event.preventDefault();
 
 		// get all form inputs in array
@@ -34,35 +69,39 @@ var SimpleFormValidator = (function() {
 			eleNameArray.push($(this).prop('name'));
 		});
 
-		$.each(config.rules, function(i, rule) {
+		console.log(config.rules);
+		
+		$.each(config.rules,
+				function(i, rule) {
 
-			var index = $.inArray(rule.fieldName, eleNameArray);
+					var index = $.inArray(rule.fieldName, eleNameArray);
 
-			if (index >= 0) {
+					if (index >= 0) {
 
-				var ele = elementsArray[index];
+						var ele = elementsArray[index];
 
-				// validation logic 1
-				if (ele.val().trim() == "" && rule.required) {
-					showErrors(ele, true, rule.message);
-					errorCount++;
-				} else {
-					showErrors(ele, false, rule.message);
-				}
+						// validation logic 1
+						if (ele.val().trim() == "" && rule.required) {
+							showErrors(ele, true, rule.message);
+							errorCount++;
+						} else {
+							showErrors(ele, false, rule.message);
+						}
 
-				// validation logic 2
-				if (rule.trimed && ele.type == 'text')
-					ele.val($.trim(ele.val()));
+						// validation logic 2
+						if (rule.trimed && ele.type == 'text')
+							ele.val($.trim(ele.val()));
 
-				// validation logic 3
-				if (ele.val().trim().length > rule.length && ele.type == 'text') {
-					showErrors(ele, true, 'Field Length Exceeds to '
-							+ ele.val().length + ' expected is ' + rule.length
-							+ '.');
-					errorCount++;
-				}
-			}
-		});
+						// validation logic 3
+						if (ele.val().trim().length > rule.length
+								&& ele.type == 'text') {
+							showErrors(ele, true, 'Field Length Exceeds to '
+									+ ele.val().length + ' expected is '
+									+ rule.length + '.');
+							errorCount++;
+						}
+					}
+				});
 
 		return errorCount;
 	}
@@ -82,18 +121,14 @@ var SimpleFormValidator = (function() {
 		}
 	}
 
-	
-	
 	function isNull(property) {
 		if (typeof (property) !== 'undefined') {
-			return true;
-		} else {
 			return false;
+		} else {
+			return true;
 		}
 	}
 
-	
-	
 	return {
 		'validate' : validate
 	}
